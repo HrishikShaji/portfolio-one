345694cc22c2611ac891e5d6337ed332cbdc3f5f
HrishikShaji <hrishikminishaji@gmail.com>
2024-01-10 22:29:18 +0530

    styles added to HEro

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,8 +1,24 @@
 export const Hero = () => {
-	return (
-		<div className="h-screen w-full flex justify-center text-white flex-col gap-4 items-center">
-			<h1 className="text-6xl font-bold ">{`Hi,I'm Hrishik.`}</h1>
-			<h1 className="text-6xl font-bold ">{`I'm a Web Developer.`}</h1>
-		</div>
-	);
+  return (
+    <div className="h-screen w-full flex justify-center text-white flex-col gap-32 items-center">
+      <div className="flex flex-col gap-4 items-center">
+        <h1 className="text-8xl font-bold ">
+          <span className="text-blue-500">{`<h1>`}</span> {`Hi,I'm Hrishik.`}
+          <span className="text-blue-500">{`<h1/>`}</span>
+        </h1>
+        <h1 className="text-6xl font-bold ">
+          <span className="text-blue-500">{`<h2>`}</span>
+          {`I'm a Web Developer.`}
+
+          <span className="text-blue-500">{`<h2/>`}</span>
+        </h1>
+      </div>
+      <h1 className="text-3xl font-bold ">
+        <span className="text-blue-500">{`<button>`}</span>
+        {`Let's Talk.`}
+
+        <span className="text-blue-500">{`<button/>`}</span>
+      </h1>
+    </div>
+  );
 };
--- app/components/Intro.tsx
+++ app/components/Intro.tsx
@@ -1,9 +1,15 @@
 export const Intro = () => {
-	return (
-		<div className="h-screen text-white w-full bg-neutral-800 gap-10 flex flex-col justify-center items-center">
-			<h1 className="text-9xl font-extrabold">I CREATE</h1>
-			<h1 className="text-9xl font-extrabold">COOL</h1>
-			<h1 className="text-9xl font-extrabold">WEB APPS</h1>
-		</div>
-	);
+  return (
+    <div className="h-full p-5 pb-20 text-white w-full bg-neutral-900 gap-1 flex flex-col justify-center items-center">
+      <div className="relative w-full justify-center flex items-center">
+        <div className="w-full origin-center top-16 h-full bg-neutral-300 mix-blend-difference absolute " />
+        <h1 className="text-9xl font-extrabold">I CREATE</h1>
+      </div>
+      <div className="relative w-full justify-center flex items-center">
+        <div className="w-full origin-center top-16 h-full bg-neutral-300 mix-blend-difference absolute " />
+        <h1 className="text-9xl  font-extrabold">COOL</h1>
+      </div>
+      <h1 className="text-9xl font-extrabold">WEB APPS</h1>
+    </div>
+  );
 };
