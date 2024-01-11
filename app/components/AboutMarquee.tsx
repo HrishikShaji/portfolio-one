a8b7bb618c40ebbac9eda980c266390fe49f2441
HrishikShaji <hrishikminishaji@gmail.com>
2024-01-11 19:32:32 +0530

    slider works but not smooth

--- app/components/AboutMarquee.tsx
+++ app/components/AboutMarquee.tsx
@@ -1,13 +1,59 @@
+"use client";
+import { useEffect, useRef } from "react";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/dist/ScrollTrigger";
+
 export const AboutMarquee = () => {
-	return (
-		<div className="w-full h-full text-white p-5 flex gap-2 overflow-hidden">
-			<h1 className="text-7xl font-semibold">ABOUT ME</h1>
-			<h1 className="text-7xl font-semibold text-red-500 inline-block">
-				ABOUT ME
-			</h1>
-			<h1 className="text-7xl font-semibold">ABOUT ME</h1>
-			<h1 className="text-7xl font-semibold">ABOUT ME</h1>
-			<h1 className="text-7xl font-semibold">ABOUT ME</h1>
-		</div>
-	);
+  const firstText = useRef(null);
+  const secondText = useRef<HTMLHeadingElement | null>(null);
+  const slider = useRef(null);
+
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    gsap.to(slider.current, {
+      scrollTrigger: {
+        trigger: document.documentElement,
+        scrub: 0.5,
+        start: 0,
+        end: window.innerHeight,
+        onUpdate: (e) => (direction = e.direction * -1),
+      },
+      x: "-500px",
+    });
+    gsap.set(secondText.current, {
+      left: secondText.current.getBoundingClientRect().width,
+    });
+    requestAnimationFrame(animate);
+  }, []);
+
+  let xPercent = 0;
+  let direction = 1;
+
+  const animate = () => {
+    if (xPercent > 0) {
+      xPercent = -100;
+    }
+    if (xPercent < -100) {
+      xPercent = 0;
+    }
+
+    gsap.set(firstText.current, { xPercent: xPercent });
+    gsap.set(secondText.current, { xPercent: xPercent });
+    requestAnimationFrame(animate);
+    xPercent += 0.1 * direction;
+  };
+
+  return (
+    <div className="overflow-hidden w-full">
+      <div ref={slider} className="relative text-white whitespace-nowrap">
+        <h1 ref={firstText} className="text-7xl relative m-0 pr-[50px]">
+          ABOUT ME ABOUT ME
+        </h1>
+        <h1 ref={secondText} className="text-7xl absolute left-[100%] top-0 ">
+          ABOUT ME ABOUT ME
+        </h1>
+      </div>
+    </div>
+  );
 };
--- app/components/Intro.tsx
+++ app/components/Intro.tsx
@@ -5,34 +5,35 @@ import ScrollTrigger from "gsap/dist/ScrollTrigger";
 import gsap from "gsap";
 
 export const Intro = () => {
-	useEffect(() => {
-		gsap.registerPlugin(ScrollTrigger);
-		const bar = document.querySelector(".bar");
-		const container = document.querySelector(".container");
-		let timeline = gsap.timeline({
-			scrollTrigger: {
-				trigger: container,
-				start: "top 70%",
-				end: "bottom 30%",
-				scrub: 1,
-				markers: true,
-			},
-			opacity: 0.2,
-		});
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    const leftBar = document.querySelector(".leftBar");
+    const rightBar = document.querySelector(".rightBar");
+    const container = document.querySelector(".container");
+    let timeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: container,
+        start: "top 70%",
+        end: "top 20%",
+        scrub: 3,
+      },
+      opacity: 0.2,
+    });
 
-		timeline.from(bar, { scaleX: 0 }).to(bar, { scale: 1 });
-	}, []);
-	return (
-		<div className="container h-full p-5 pb-20 text-white w-full bg-neutral-900 gap-1 flex flex-col justify-center items-center">
-			<div className="relative w-full justify-center flex items-center">
-				<div className="bar w-full  origin-left top-16 h-full bg-neutral-300 mix-blend-difference absolute " />
-				<h1 className="text-9xl font-extrabold">I CREATE</h1>
-			</div>
-			<div className="relative w-full justify-center flex items-center">
-				<div className="w-full origin-center top-16 h-full bg-neutral-300 mix-blend-difference absolute " />
-				<h1 className="text-9xl  font-extrabold">COOL</h1>
-			</div>
-			<h1 className="text-9xl font-extrabold">WEB APPS</h1>
-		</div>
-	);
+    timeline.from(leftBar, { scaleX: 0 }).to(leftBar, { scale: 1 });
+    timeline.from(rightBar, { scaleX: 0 }).to(rightBar, { scale: 1 });
+  }, []);
+  return (
+    <div className="container h-full p-5 pb-20 text-white w-full bg-neutral-900 gap-1 flex flex-col justify-center items-center">
+      <div className="relative w-full justify-center flex items-center">
+        <div className="leftBar w-full  origin-left top-16 h-full bg-neutral-300 mix-blend-difference absolute " />
+        <h1 className="text-9xl font-extrabold">I CREATE</h1>
+      </div>
+      <div className="relative w-full justify-center flex items-center">
+        <div className="rightBar w-full origin-right top-16 h-full bg-neutral-300 mix-blend-difference absolute " />
+        <h1 className="text-9xl  font-extrabold">COOL</h1>
+      </div>
+      <h1 className="text-9xl font-extrabold">WEB APPS</h1>
+    </div>
+  );
 };
