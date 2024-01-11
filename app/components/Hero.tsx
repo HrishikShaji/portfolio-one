7eaf2349449c78f20a9e830d31173009a80fa203
HrishikShaji <hrishikminishaji@gmail.com>
2024-01-11 12:21:26 +0530

    added scrolltrigger

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,6 +1,6 @@
 export const Hero = () => {
   return (
-    <div className="h-screen w-full flex justify-center text-white flex-col gap-32 items-center">
+    <div className="h-screen w-full flex justify-center text-white flex-col gap-40 items-center">
       <div className="flex flex-col gap-4 items-center">
         <h1 className="text-8xl font-bold ">
           <span className="text-blue-500">{`<h1>`}</span> {`Hi,I'm Hrishik.`}
@@ -13,7 +13,7 @@ export const Hero = () => {
           <span className="text-blue-500">{`<h2/>`}</span>
         </h1>
       </div>
-      <h1 className="text-3xl font-bold ">
+      <h1 className="text-2xl font-bold ">
         <span className="text-blue-500">{`<button>`}</span>
         {`Let's Talk.`}
 
--- app/components/Intro.tsx
+++ app/components/Intro.tsx
@@ -1,15 +1,38 @@
+"use client";
+
+import { useEffect } from "react";
+import ScrollTrigger from "gsap/dist/ScrollTrigger";
+import gsap from "gsap";
+
 export const Intro = () => {
-  return (
-    <div className="h-full p-5 pb-20 text-white w-full bg-neutral-900 gap-1 flex flex-col justify-center items-center">
-      <div className="relative w-full justify-center flex items-center">
-        <div className="w-full origin-center top-16 h-full bg-neutral-300 mix-blend-difference absolute " />
-        <h1 className="text-9xl font-extrabold">I CREATE</h1>
-      </div>
-      <div className="relative w-full justify-center flex items-center">
-        <div className="w-full origin-center top-16 h-full bg-neutral-300 mix-blend-difference absolute " />
-        <h1 className="text-9xl  font-extrabold">COOL</h1>
-      </div>
-      <h1 className="text-9xl font-extrabold">WEB APPS</h1>
-    </div>
-  );
+	useEffect(() => {
+		gsap.registerPlugin(ScrollTrigger);
+		const bar = document.querySelector(".bar");
+		const container = document.querySelector(".container");
+		let timeline = gsap.timeline({
+			scrollTrigger: {
+				trigger: container,
+				start: "top 70%",
+				end: "bottom 30%",
+				scrub: 1,
+				markers: true,
+			},
+			opacity: 0.2,
+		});
+
+		timeline.from(bar, { scaleX: 0 }).to(bar, { scale: 1 });
+	}, []);
+	return (
+		<div className="container h-full p-5 pb-20 text-white w-full bg-neutral-900 gap-1 flex flex-col justify-center items-center">
+			<div className="relative w-full justify-center flex items-center">
+				<div className="bar w-full  origin-left top-16 h-full bg-neutral-300 mix-blend-difference absolute " />
+				<h1 className="text-9xl font-extrabold">I CREATE</h1>
+			</div>
+			<div className="relative w-full justify-center flex items-center">
+				<div className="w-full origin-center top-16 h-full bg-neutral-300 mix-blend-difference absolute " />
+				<h1 className="text-9xl  font-extrabold">COOL</h1>
+			</div>
+			<h1 className="text-9xl font-extrabold">WEB APPS</h1>
+		</div>
+	);
 };
