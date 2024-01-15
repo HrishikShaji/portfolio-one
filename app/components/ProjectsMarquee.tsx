2076f1028f71d7faf9b27fe103a739c4f8e25a49
HrishikShaji <hrishikminishaji@gmail.com>
2024-01-15 23:28:56 +0530

    marquee direction change works

--- app/components/ProjectsMarquee.tsx
+++ app/components/ProjectsMarquee.tsx
@@ -5,41 +5,40 @@ import gsap from "gsap";
 import ScrollTrigger from "gsap/dist/ScrollTrigger";
 
 export const ProjectsMarquee = () => {
-  const slider = useRef<HTMLDivElement | null>(null);
+	const slider = useRef<HTMLDivElement | null>(null);
 
-  useEffect(() => {
-    gsap.registerPlugin(ScrollTrigger);
-    gsap.to(slider.current, {
-      scrollTrigger: {
-        trigger: slider.current,
-        scrub: 5,
-        start: "top bottom",
-        end: "top top",
-        markers: true,
-      },
-      x: "30%",
-    });
-  }, []);
-  return (
-    <div className="h-[300px] relative w-full overflow-hidden">
-      <div
-        ref={slider}
-        className="w-full absolute -left-[25%] h-full text-white p-5 flex gap-2 "
-      >
-        <h1 className="text-7xl font-semibold">MY PROJECTS</h1>
-        <h1 className="text-7xl font-semibold">MY PROJECTS</h1>
-        <h1 className="text-7xl font-semibold">MY PROJECTS</h1>
-        <h1 className="text-7xl font-semibold">MY PROJECTS</h1>
-        <h1 className="text-7xl font-semibold text-red-500 inline-block">
-          MY PROJECTS
-        </h1>
-        <h1 className="text-7xl font-semibold">MY PROJECTS</h1>
-        <h1 className="text-7xl font-semibold">MY PROJECTS</h1>
-        <h1 className="text-7xl font-semibold">MY PROJECTS</h1>
-        <h1 className="text-7xl font-semibold">MY PROJECTS</h1>
-        <h1 className="text-7xl font-semibold">MY PROJECTS</h1>
-        <h1 className="text-7xl font-semibold">MY PROJECTS</h1>
-      </div>
-    </div>
-  );
+	useEffect(() => {
+		gsap.registerPlugin(ScrollTrigger);
+		gsap.to(slider.current, {
+			scrollTrigger: {
+				trigger: slider.current,
+				scrub: 5,
+				start: "top bottom",
+				end: "top top",
+			},
+			x: "30%",
+		});
+	}, []);
+	return (
+		<div className="h-[300px] relative w-full overflow-hidden">
+			<div
+				ref={slider}
+				className="w-full absolute -left-[25%] h-full text-white p-5 flex gap-2 "
+			>
+				<h1 className="text-7xl font-semibold">MY PROJECTS</h1>
+				<h1 className="text-7xl font-semibold">MY PROJECTS</h1>
+				<h1 className="text-7xl font-semibold">MY PROJECTS</h1>
+				<h1 className="text-7xl font-semibold">MY PROJECTS</h1>
+				<h1 className="text-7xl font-semibold text-red-500 inline-block">
+					MY PROJECTS
+				</h1>
+				<h1 className="text-7xl font-semibold">MY PROJECTS</h1>
+				<h1 className="text-7xl font-semibold">MY PROJECTS</h1>
+				<h1 className="text-7xl font-semibold">MY PROJECTS</h1>
+				<h1 className="text-7xl font-semibold">MY PROJECTS</h1>
+				<h1 className="text-7xl font-semibold">MY PROJECTS</h1>
+				<h1 className="text-7xl font-semibold">MY PROJECTS</h1>
+			</div>
+		</div>
+	);
 };
--- app/components/SkillsMarquee.tsx
+++ app/components/SkillsMarquee.tsx
@@ -2,21 +2,38 @@
 
 import { useEffect, useRef } from "react";
 import gsap from "gsap";
+import ScrollTrigger from "gsap/dist/ScrollTrigger";
 
 export const SkillsMarquee = () => {
 	const firstText = useRef(null);
 	const secondText = useRef(null);
+	const slider = useRef(null);
 
 	let xPercent = 0;
 	let direction = -1;
 
 	useEffect(() => {
+		gsap.registerPlugin(ScrollTrigger);
 		requestAnimationFrame(animation);
+
+		gsap.to(slider.current, {
+			scrollTrigger: {
+				trigger: slider.current,
+				start: "top bottom",
+				end: "top top",
+				scrub: true,
+				markers: true,
+				onUpdate: (e) => (direction = e.direction * -1),
+			},
+		});
 	}, []);
 
 	const animation = () => {
 		if (xPercent <= -100) {
 			xPercent = 0;
+		}
+		if (xPercent > 0) {
+			xPercent = -100;
 		}
 		gsap.set(firstText.current, { xPercent: xPercent });
 		gsap.set(secondText.current, { xPercent: xPercent });
@@ -26,7 +43,7 @@ export const SkillsMarquee = () => {
 
 	return (
 		<div className="absolute">
-			<div className="relative whitespace-nowrap">
+			<div ref={slider} className="relative whitespace-nowrap">
 				<p
 					ref={firstText}
 					className="text-7xl relative m-0  text-white font-bold"
