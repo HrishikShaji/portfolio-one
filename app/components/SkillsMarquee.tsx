64a3151c814268612ea86402945d9ca458a0581f
HrishikShaji <hrishikminishaji@gmail.com>
2024-01-15 22:44:15 +0530

    infinite marquee works in skills

--- app/components/SkillsMarquee.tsx
+++ app/components/SkillsMarquee.tsx
@@ -1,13 +1,45 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+import gsap from "gsap";
+
 export const SkillsMarquee = () => {
-  return (
-    <div className="w-full h-full text-white p-5 flex gap-2 overflow-hidden">
-      <h1 className="text-7xl font-semibold">MY SKILLS</h1>
-      <h1 className="text-7xl font-semibold text-red-500 inline-block">
-        MY SKILLS
-      </h1>
-      <h1 className="text-7xl font-semibold">MY SKILLS</h1>
-      <h1 className="text-7xl font-semibold">MY SKILLS</h1>
-      <h1 className="text-7xl font-semibold">MY SKILLS</h1>
-    </div>
-  );
+	const firstText = useRef(null);
+	const secondText = useRef(null);
+
+	let xPercent = 0;
+	let direction = -1;
+
+	useEffect(() => {
+		requestAnimationFrame(animation);
+	}, []);
+
+	const animation = () => {
+		if (xPercent <= -100) {
+			xPercent = 0;
+		}
+		gsap.set(firstText.current, { xPercent: xPercent });
+		gsap.set(secondText.current, { xPercent: xPercent });
+		xPercent += 0.1 * direction;
+		requestAnimationFrame(animation);
+	};
+
+	return (
+		<div className="absolute">
+			<div className="relative whitespace-nowrap">
+				<p
+					ref={firstText}
+					className="text-7xl relative m-0  text-white font-bold"
+				>
+					MY SKILLS MY SKILLS MY SKILLS MY SKILLS
+				</p>
+				<p
+					ref={secondText}
+					className="text-7xl text-white font-bold absolute left-[100%] top-0"
+				>
+					MY SKILLS MY SKILLS MY SKILLS MY SKILLS
+				</p>
+			</div>
+		</div>
+	);
 };
