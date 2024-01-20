b7cdeca79dc602423f9f9b1bad3e8b463e6ccb28
HrishikShaji <hrishikminishaji@gmail.com>
2024-01-20 20:08:48 +0530

    framer-motion added

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,24 +1,34 @@
+"use client";
+
+import { motion } from "framer-motion";
+
 export const Hero = () => {
-  return (
-    <div className="h-screen w-full flex justify-center text-white flex-col gap-40 items-center">
-      <div className="flex flex-col gap-4 items-center">
-        <h1 className="text-8xl font-bold ">
-          <span className="text-blue-500">{`<h1>`}</span> {`Hi,I'm Hrishik.`}
-          <span className="text-blue-500">{`<h1/>`}</span>
-        </h1>
-        <h1 className="text-6xl font-bold ">
-          <span className="text-blue-500">{`<h2>`}</span>
-          {`I'm a Web Developer.`}
+	return (
+		<div className="h-screen w-full flex justify-center text-white flex-col gap-40 items-center">
+			<div className="flex flex-col gap-4 items-center">
+				<motion.h1
+					initial={{ x: 300, opacity: 0 }}
+					animate={{ x: 0, opacity: 1 }}
+					transition={{ duration: 1 }}
+					className="text-8xl font-bold "
+				>
+					<span id="left-tag-one" className="text-blue-500">{`<h1>`}</span>{" "}
+					{`Hi,I'm Hrishik.`}
+					<span id="right-tag-one" className="text-blue-500">{`<h1/>`}</span>
+				</motion.h1>
+				<h1 className="text-6xl font-bold ">
+					<span className="text-blue-500">{`<h2>`}</span>
+					{`I'm a Web Developer.`}
 
-          <span className="text-blue-500">{`<h2/>`}</span>
-        </h1>
-      </div>
-      <h1 className="text-2xl font-bold ">
-        <span className="text-blue-500">{`<button>`}</span>
-        {`Let's Talk.`}
+					<span className="text-blue-500">{`<h2/>`}</span>
+				</h1>
+			</div>
+			<h1 className="text-2xl font-bold ">
+				<span className="text-blue-500">{`<button>`}</span>
+				{`Let's Talk.`}
 
-        <span className="text-blue-500">{`<button/>`}</span>
-      </h1>
-    </div>
-  );
+				<span className="text-blue-500">{`<button/>`}</span>
+			</h1>
+		</div>
+	);
 };
--- app/components/Intro.tsx
+++ app/components/Intro.tsx
@@ -7,51 +7,53 @@ import gsap from "gsap";
 const data = ["I CREATE", "COOL", "WEB APPS"];
 
 export const Intro = () => {
-	useEffect(() => {
-		gsap.registerPlugin(ScrollTrigger);
-		document.querySelectorAll(".bar").forEach((bar) => {
-			const tl = gsap.timeline({
-				scrollTrigger: {
-					trigger: bar,
-					start: "top 90%",
-					end: "top 40%",
-					scrub: 1,
-				},
-			});
-			tl.fromTo(
-				bar,
-				{
-					backgroundColor: "blue",
-					scaleX: 0,
-					opacity: 0,
-					transformOrigin: "left",
-				},
-				{
-					scaleX: 1,
-					opacity: 1,
-					backgroundColor: "white",
-				},
-			).to(bar, {
-				backgroundColor: "blue",
-				scaleX: 0,
-				opacity: 0,
-				transformOrigin: "right",
-			});
-		});
-	}, []);
-	return (
-		<div className="container h-screen  text-white w-full bg-neutral-900  flex flex-col justify-center items-center">
-			{data.map((item, i) => (
-				<div key={i} className="relative w-full justify-start flex ">
-					<div className="h-full absolute w-full flex flex-col  mix-blend-difference">
-						<div className="leftBar bar w-full  origin-left h-[25%]" />
-						<div className="leftBar bar w-full  origin-left h-[25%]  " />
-						<div className="leftBar bar w-full  origin-left h-[25%] " />
-						<div className="leftBar bar w-full  origin-left h-[25%] " />
-					</div>
-					<h1 className="text-[150px] leading-none ">{item}</h1>
-				</div>
-			))}
-		</div>
-	);
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    document.querySelectorAll(".bar").forEach((bar) => {
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: bar,
+          start: "top 90%",
+          end: "top 40%",
+          scrub: 2.5,
+        },
+      });
+      tl.fromTo(
+        bar,
+        {
+          backgroundColor: "blue",
+          scaleX: 0,
+          opacity: 0,
+          transformOrigin: "left",
+        },
+        {
+          scaleX: 1,
+          opacity: 1,
+          backgroundColor: "white",
+        },
+      ).to(bar, {
+        backgroundColor: "blue",
+        scaleX: 0,
+        opacity: 0,
+        transformOrigin: "right",
+      });
+    });
+  }, []);
+  return (
+    <div className="container h-screen  text-white w-full bg-neutral-900  flex flex-col justify-center items-center">
+      {data.map((item, i) => (
+        <div key={i} className="relative w-full justify-start flex ">
+          <div className="h-full absolute w-full flex flex-col  mix-blend-difference">
+            {Array.from({ length: 5 }).map((_, i) => (
+              <div
+                key={i}
+                className="leftBar bar w-full  origin-left h-[20%]"
+              />
+            ))}
+          </div>
+          <h1 className="text-[150px] leading-none ">{item}</h1>
+        </div>
+      ))}
+    </div>
+  );
 };
