f56af70cd9997a67f3b07a1e97c1f86f4f41f847
HrishikShaji <hrishikminishaji@gmail.com>
2024-01-22 11:42:02 +0530

    boxes working

--- app/components/Features.tsx
+++ app/components/Features.tsx
@@ -4,64 +4,88 @@ import gsap from "gsap";
 import ScrollTrigger from "gsap/dist/ScrollTrigger";
 import { useEffect, useRef, useState } from "react";
 
+const data = ["CLEAN CODE", "REUSABLE COMPONENTS", "RESPONSIVE DESIGN"];
+
+interface BoxProps {
+	item: string;
+	index: number;
+}
+const Box: React.FC<BoxProps> = ({ item, index }) => {
+	const [isMounted, setIsMounted] = useState(false);
+	useEffect(() => {
+		setIsMounted(true);
+	}, []);
+	useEffect(() => {
+		gsap.registerPlugin(ScrollTrigger);
+		const box = document.getElementById(`box-${index}`);
+		const overlay = document.getElementById(`overlay-${index}`);
+		if (isMounted) {
+			gsap.fromTo(
+				overlay,
+				{ scaleY: 0 },
+				{
+					scaleY: 1,
+					transformOrigin: "top",
+					scrollTrigger: {
+						trigger: box,
+						start: "top center",
+						end: "center center",
+						scrub: .5,
+						markers: true,
+					},
+				},
+			);
+		}
+	}, [isMounted]);
+	return (
+		<div
+			id={`box-${index}`}
+			className="box h-[300px] w-[300px] relative rounded-md  flex justify-center items-center"
+		>
+			<h1 className="text-4xl font-bold mix-blend-difference z-10">{item}</h1>
+			<div
+				id={`overlay-${index}`}
+				className="overlay absolute top-0 h-full w-full bg-white"
+			/>
+		</div>
+	);
+};
+
 export const Features = () => {
-  const [isMounted, setIsMounted] = useState(false);
-  useEffect(() => {
-    setIsMounted(true);
-  }, []);
-  useEffect(() => {
-    gsap.registerPlugin(ScrollTrigger);
-    const pin = document.querySelector(".pin-ref");
-    const tar = document.querySelector(".target-ref");
-    const box = document.querySelector(".box");
-    if (isMounted) {
-      gsap.to(pin, {
-        scrollTrigger: {
-          trigger: tar,
-          pin: pin,
-          start: "top top",
-          end: "bottom bottom",
-        },
-      });
+	const [isMounted, setIsMounted] = useState(false);
+	useEffect(() => {
+		setIsMounted(true);
+	}, []);
+	useEffect(() => {
+		gsap.registerPlugin(ScrollTrigger);
+		const pin = document.querySelector(".pin-ref");
+		const tar = document.querySelector(".target-ref");
+		if (isMounted) {
+			gsap.to(pin, {
+				scrollTrigger: {
+					trigger: tar,
+					pin: pin,
+					start: "top top",
+					end: "bottom bottom",
+				},
+			});
+		}
+	}, [isMounted]);
 
-      gsap.fromTo(
-        box,
-        { backgroundColor: "black" },
-        {
-          backgroundColor: "red",
-          scrollTrigger: {
-            trigger: box,
-            start: "top center",
-            end: "bottom top",
-            scrub: true,
-            markers: true,
-          },
-        },
-      );
-      console.log(pin, tar);
-    }
-  }, [isMounted]);
-
-  return (
-    <div className="h-full w-full flex text-white ">
-      <div className="pin-ref h-screen w-[50%] bg-neutral-600 flex justify-center items-center">
-        <div className="flex flex-col gap-0 leading-none text-9xl items-start">
-          <h1 className="text-[250px]">{`I'm`}</h1>
-          <h1>Best</h1>
-          <h1>At</h1>
-        </div>
-      </div>
-      <div className="target-ref flex flex-col gap-4 bg-neutral-400 w-[50%] h-[300vh] items-center justify-around">
-        <div className="box h-[300px] w-[300px] rounded-md bg-black flex justify-center items-center">
-          <h1 className="text-4xl font-bold">CLEAN CODE</h1>
-        </div>
-        <div className="h-[300px] w-[300px] rounded-md bg-black flex justify-center items-center">
-          <h1 className="text-4xl font-bold">REUSABLE COMPONENTS</h1>
-        </div>
-        <div className="h-[300px] w-[300px] rounded-md bg-black flex justify-center items-center">
-          <h1 className="text-4xl font-bold">RESPONSIVE DESIGN</h1>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="h-full w-full flex text-white ">
+			<div className="pin-ref h-screen w-[50%]  flex justify-center items-center">
+				<div className="flex flex-col gap-0 leading-none text-9xl items-start">
+					<h1 className="text-[250px]">{`I'm`}</h1>
+					<h1>Best</h1>
+					<h1>At</h1>
+				</div>
+			</div>
+			<div className="target-ref flex flex-col gap-4  w-[50%] h-[300vh] items-center justify-around">
+				{data.map((item, i) => (
+					<Box item={item} index={i} key={i} />
+				))}
+			</div>
+		</div>
+	);
 };
