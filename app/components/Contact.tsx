4e638b6d7812f8a532fb49d367696d32f0cf6698
HrishikShaji <hrishikminishaji@gmail.com>
2024-01-22 19:48:01 +0530

    improved form

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -1,6 +1,42 @@
+"use client";
+import { useEffect, useState } from "react";
 import { Marquee } from "./Marquee";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/dist/ScrollTrigger";
 
 export const Contact = () => {
+	const [isMounted, setIsMounted] = useState(false);
+
+	useEffect(() => {
+		setIsMounted(true);
+	}, []);
+
+	useEffect(() => {
+		gsap.registerPlugin(ScrollTrigger);
+		const inputs = gsap.utils.toArray(".input");
+
+		inputs.forEach((input: any) => {
+			const anim = gsap.fromTo(
+				input,
+				{
+					autoAlpha: 0,
+					y: 50,
+				},
+				{
+					duration: 1,
+					autoAlpha: 1,
+					y: 0,
+					delay: 0.5,
+				},
+			);
+			ScrollTrigger.create({
+				trigger: input,
+				animation: anim,
+				toggleActions: "play none none none",
+				once: true,
+			});
+		});
+	}, [isMounted]);
 	return (
 		<div
 			id="contact"
@@ -9,23 +45,23 @@ export const Contact = () => {
 			<Marquee text="CONTACT ME" number={2} colorIndex={1} />
 
 			<div className="flex flex-col gap-10">
-				<h1 className="text-3xl font-bold">{`Let's Discuss What You Want?`}</h1>
+				<h1 className="input text-3xl font-bold">{`Let's Discuss What You Want?`}</h1>
 				<form className="flex flex-col gap-3 items-center ">
 					<input
-						className="border-b-2 w-full border-red-500 bg-neutral-700 p-2"
+						className="input border-b-2 w-full border-white p-2 bg-transparent"
 						type="text"
 						placeholder="Name..."
 					/>
 					<input
-						className="border-b-2 border-red-500 bg-neutral-700 p-2 w-full"
+						className="input border-b-2 border-white p-2 w-full bg-transparent"
 						type="email"
 						placeholder="Email..."
 					/>
 					<textarea
-						className="border-b-2 border-red-500 bg-neutral-700 p-2 w-full"
-						placeholder="Name..."
+						className="input border-b-2 border-white bg-transparent p-2 w-full"
+						placeholder="Message..."
 					/>
-					<button className="w-full p-2 bg-red-500 text-black text-xl border-2 border-white">
+					<button className="input w-full p-2 bg-white text-black text-xl ">
 						SEND
 					</button>
 				</form>
