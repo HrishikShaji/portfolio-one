261d7d84074bed18aea74bf0ec00c9d2988b25e7
HrishikShaji <hrishikminishaji@gmail.com>
2024-03-12 23:01:32 +0530

    modified feature

--- app/components/Box.tsx
+++ app/components/Box.tsx
@@ -1,45 +1,40 @@
 "use client";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/dist/ScrollTrigger";
 import { useMount } from "../hooks/useMount";
 
 interface BoxProps {
   item: string;
-  index: number;
 }
-export const Box: React.FC<BoxProps> = ({ item, index }) => {
-  const { isMounted } = useMount();
+export const Box: React.FC<BoxProps> = ({ item }) => {
+  const boxRef = useRef<HTMLDivElement>(null);
+  const overlayRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    gsap.registerPlugin(ScrollTrigger);
-    if (isMounted) {
-      const box = document.getElementById(`box-${index}`);
-      const overlay = document.getElementById(`overlay-${index}`);
-      gsap.fromTo(
-        overlay,
-        { scaleY: 0 },
-        {
-          scaleY: 1,
-          transformOrigin: "top",
-          scrollTrigger: {
-            trigger: box,
-            start: "top center",
-            end: "center center",
-            scrub: 0.5,
-          },
+    gsap.fromTo(
+      overlayRef.current,
+      { scaleY: 0 },
+      {
+        scaleY: 1,
+        transformOrigin: "top",
+        scrollTrigger: {
+          trigger: boxRef.current,
+          start: "top center",
+          end: "center center",
+          scrub: 0.5,
         },
-      );
-    }
-  }, [isMounted, index]);
+      },
+    );
+  }, []);
   return (
     <div
-      id={`box-${index}`}
+      ref={boxRef}
       className="box h-[300px] w-[300px] relative rounded-md  flex justify-center text-center items-center"
     >
       <h1 className="text-4xl font-bold mix-blend-difference z-10">{item}</h1>
       <div
-        id={`overlay-${index}`}
+        ref={overlayRef}
         className="overlay absolute top-0 h-full w-full bg-white"
       />
     </div>
--- app/components/Features.tsx
+++ app/components/Features.tsx
@@ -6,43 +6,43 @@ import { Box } from "./Box";
 import { data } from "../lib/data";
 
 export const Features = () => {
-	const pinRef = useRef<HTMLDivElement>(null);
-	const targetRef = useRef<HTMLDivElement>(null);
-	useLayoutEffect(() => {
-		let ctx = gsap.context(() => {
-			gsap.to(pinRef.current, {
-				scrollTrigger: {
-					trigger: targetRef.current,
-					pin: pinRef.current,
-					start: "top top",
-					end: "bottom bottom",
-				},
-			});
-		});
+  const pinRef = useRef<HTMLDivElement>(null);
+  const targetRef = useRef<HTMLDivElement>(null);
+  useLayoutEffect(() => {
+    let ctx = gsap.context(() => {
+      gsap.to(pinRef.current, {
+        scrollTrigger: {
+          trigger: targetRef.current,
+          pin: pinRef.current,
+          start: "top top",
+          end: "bottom bottom",
+        },
+      });
+    });
 
-		return () => ctx.revert();
-	}, []);
+    return () => ctx.revert();
+  }, []);
 
-	return (
-		<div className="h-full w-full flex text-white ">
-			<div
-				ref={pinRef}
-				className=" h-screen w-[50%]  flex justify-center items-center"
-			>
-				<div className="flex flex-col gap-0 leading-none text-9xl items-start">
-					<h1 className="text-[250px]">{`I'm`}</h1>
-					<h1>Best</h1>
-					<h1>At</h1>
-				</div>
-			</div>
-			<div
-				ref={targetRef}
-				className=" flex flex-col gap-4  w-[50%] h-[300vh] items-center justify-around"
-			>
-				{data.features.data.map((item, i) => (
-					<Box item={item} index={i} key={i} />
-				))}
-			</div>
-		</div>
-	);
+  return (
+    <div className="h-full w-full flex text-white ">
+      <div
+        ref={pinRef}
+        className=" h-screen w-[50%]  flex justify-center items-center"
+      >
+        <div className="flex flex-col gap-0 leading-none text-9xl items-start">
+          <h1 className="text-[250px]">{`I'm`}</h1>
+          <h1>Best</h1>
+          <h1>At</h1>
+        </div>
+      </div>
+      <div
+        ref={targetRef}
+        className=" flex flex-col gap-4  w-[50%] h-[300vh] items-center justify-around"
+      >
+        {data.features.data.map((item, i) => (
+          <Box item={item} key={i} />
+        ))}
+      </div>
+    </div>
+  );
 };
