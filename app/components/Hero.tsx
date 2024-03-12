e1452ab268947d7476c7b87614a22fa832a02e1b
HrishikShaji <hrishikminishaji@gmail.com>
2024-03-12 11:13:10 +0530

    modified splitext

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,64 +1,24 @@
 "use client";
 
-import gsap from "gsap";
-import { useEffect, useState } from "react";
-import { Code } from "./Code";
-import { useMount } from "../hooks/useMount";
 import { data } from "../lib/data";
+import { SplitText } from "./SplitText";
 
 export const Hero = () => {
-  const [code, setCode] = useState(true);
-  const { isMounted } = useMount();
-  const splitWord = (word: string) => {
-    return word.split("").map((letter, i) => (
-      <div
-        className="text-animate translate-y-[220px] transition transform duration-700"
-        key={i}
-      >
-        {letter}
-      </div>
-    ));
-  };
-
-  useEffect(() => {
-    if (isMounted) {
-      gsap.to(".text-animate", {
-        y: 0,
-        stagger: 0.05,
-        duration: 0.05,
-        ease: "bounce",
-      });
-    }
-  }, [isMounted, code]);
-
   return (
     <div
       id={data.hero.id}
       className="h-screen w-full flex justify-center text-white flex-col gap-40 items-center"
     >
       <div className="flex flex-col gap-4 items-center">
-        {code ? (
-          <Code setCode={setCode} />
-        ) : (
-          <div className="flex flex-col gap-20">
-            <div className="flex flex-col gap-2" style={{ whiteSpace: "pre" }}>
-              <h1 className="custom-clip text-8xl font-bold flex">
-                {splitWord(data.hero.headingOne)}
-              </h1>
-              <h1 className="custom-clip text-8xl font-bold flex">
-                {splitWord(data.hero.headingTwo)}
-              </h1>
-            </div>
-            <button>
-              <h1
-                className="custom-clip text-3xl flex"
-                style={{ whiteSpace: "pre" }}
-              >
-                {splitWord("Let's Talk")}
-              </h1>
-            </button>
+        <div className="flex flex-col gap-20">
+          <div className="flex flex-col gap-2">
+            <SplitText text={data.hero.headingOne} />
+            <SplitText text={data.hero.headingTwo} />
           </div>
-        )}
+          <button>
+            <SplitText text={`Let's Talk`} />
+          </button>
+        </div>
       </div>
     </div>
   );
---
+++ app/components/SplitText.tsx
@@ -0,0 +1,38 @@
+import gsap from "gsap";
+import { useLayoutEffect, useRef } from "react";
+
+interface SplitTextProps {
+  text: string;
+}
+
+export const SplitText: React.FC<SplitTextProps> = ({ text }) => {
+  const letterRefs = useRef<(HTMLDivElement | null)[]>([]);
+  useLayoutEffect(() => {
+    let ctx = gsap.context(() => {
+      gsap.set(letterRefs.current, { y: 500 });
+      gsap.to(letterRefs.current, {
+        delay: 1,
+        y: 0,
+        stagger: 0.05,
+        duration: 0.05,
+        ease: "bounce",
+      });
+    });
+
+    return () => ctx.revert();
+  }, []);
+
+  return (
+    <h1 className="custom-clip text-6xl flex" style={{ whiteSpace: "pre" }}>
+      {text.split("").map((letter, i) => (
+        <div
+          ref={(el) => (letterRefs.current[i] = el)}
+          className="text-animate  transition transform duration-700"
+          key={i}
+        >
+          {letter}
+        </div>
+      ))}
+    </h1>
+  );
+};
