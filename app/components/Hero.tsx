2823bc1359942eff746c2f42923f34e192b271da
HrishikShaji <hrishikminishaji@gmail.com>
2024-03-13 22:56:19 +0530

    toggle animation logic works

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -7,31 +7,31 @@ import { SplitText } from "./SplitText";
 const speed = [0.1, 0.2, 0.1, 0.2, 0.1, 0.2, 0.1, 0.2];
 
 export const Hero = () => {
-  return (
-    <div
-      id={data.hero.id}
-      className="h-screen w-full pt-20 flex relative  flex-col  "
-    >
-      {Array.from({ length: 7 }).map((_, j) => (
-        <Marquee key={j} speed={speed[j]}>
-          {Array.from({ length: 5 }).map((_, i) => (
-            <h1 key={i} className="text-neutral-800 font-audiowide text-8xl ">
-              ANAIKIN <span className="text-red-500">.</span>
-            </h1>
-          ))}
-        </Marquee>
-      ))}
-      <div className="flex flex-col gap-4 items-center absolute top-40 left-10 text-white">
-        <div className="flex flex-col gap-20">
-          <div className="flex flex-col gap-2 text-8xl bg-red-500 text-black p-10 rounded-3xl">
-            <SplitText text={`ANAKIN SKYWALKER.`} />
-            <SplitText text="WEB DEVELOPER." />
-          </div>
-          <button className="text-6xl text-red-500">
-            <SplitText text={`Let's Talk`} />
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div
+			id={data.hero.id}
+			className="h-screen w-full pt-20 flex relative  flex-col  "
+		>
+			{Array.from({ length: 7 }).map((_, j) => (
+				<Marquee key={j} speed={speed[j]}>
+					{Array.from({ length: 5 }).map((_, i) => (
+						<h1 key={i} className="text-neutral-800 font-audiowide text-8xl ">
+							ANAIKIN <span className="text-red-500">.</span>
+						</h1>
+					))}
+				</Marquee>
+			))}
+			<div className="flex flex-col gap-4 items-center absolute top-[170px] left-10 text-white">
+				<div className="flex flex-col gap-20">
+					<div className="flex flex-col gap-2 text-8xl bg-red-500 text-black p-10 rounded-3xl">
+						<SplitText text={`ANAKIN SKYWALKER.`} />
+						<SplitText text="WEB DEVELOPER." />
+					</div>
+					<button className="text-6xl text-red-500">
+						<SplitText text={`Let's Talk`} />
+					</button>
+				</div>
+			</div>
+		</div>
+	);
 };
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,12 +1,37 @@
 "use client";
 
+import gsap from "gsap";
 import Link from "next/link";
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
 
 export const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const menuRef = useRef<HTMLDivElement>(null);
+  const buttonRef = useRef<HTMLButtonElement>(null);
+  const timeline = useRef();
+
+  useEffect(() => {
+    timeline.current = gsap.timeline({ paused: true });
+    gsap.set(menuRef.current, { yPercent: 100 });
+    timeline.current.to(menuRef.current, {
+      yPercent: 0,
+      duration: 2,
+    });
+  }, []);
+
+  useEffect(() => {
+    console.log(isOpen);
+    isOpen ? timeline.current.play() : timeline.current.reverse();
+  }, [isOpen]);
   return (
-    <div className="bg-neutral-900 text-white  fixed z-50 top-0 w-full h-[80px] p-5 flex justify-between">
-      <h1>HRISHIK</h1>
-      <div className="flex gap-4">
+    <div className=" text-white  fixed z-50 top-0 w-full   flex flex-col">
+      <div className="p-2 bg-red-500 flex gap-2">
+        <button onClick={() => setIsOpen(!isOpen)} ref={buttonRef}>
+          MENU
+        </button>
+        <h1 className="font-audiowide text-2xl">HRISHIK</h1>
+      </div>
+      <div className="flex  flex-grow bg-red-500 gap-4 h-screen" ref={menuRef}>
         <Link href="#home">Home</Link>
         <Link href="#about">About</Link>
         <Link href="#skills">Skills</Link>
