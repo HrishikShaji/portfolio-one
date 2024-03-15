72f6efc461eacfe88cc85d2b29e94acad254824b
HrishikShaji <hrishikminishaji@gmail.com>
2024-03-15 23:04:45 +0530

    canvas started

--- app/components/FlowChart.tsx
+++ app/components/FlowChart.tsx
@@ -1,10 +1,18 @@
 export const FlowChart = () => {
 	return (
-		<div className="container grid grid-cols-4 gap-40 ">
-			<div className="box  ">1</div>
-			<div className="box">2</div>
-			<div className="box">3</div>
-			<div className="box">4</div>
+		<div className="container flex flex-col items-center justify-center gap-40 ">
+			<canvas className="border-black border-2"></canvas>
+			<div className="grid grid-cols-3 gap-40">
+				<div className="flex main-box relative items-center gap-[100px]">
+					<div className="box  ">1</div>
+					<div className="flex flex-col gap-10">
+						<div className="box  ">1</div>
+						<div className="box  ">1</div>
+					</div>
+				</div>
+				<div className="box  ">1</div>
+				<div className="box">2</div>
+			</div>
 		</div>
 	);
 };
