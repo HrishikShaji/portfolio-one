a409d134ec0c773d34f07039c8b451219039a976
HrishikShaji <hrishikminishaji@gmail.com>
2024-03-24 22:42:36 +0530

    modified projectcard animation

--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -4,67 +4,67 @@ import Image from "next/image";
 import { useLayoutEffect, useRef } from "react";
 
 interface ProjectCardProps {
-	title: string;
-	img: string;
+  title: string;
+  img: string;
 }
 
 export const ProjectCard: React.FC<ProjectCardProps> = ({ title, img }) => {
-	const cardRef = useRef<HTMLDivElement>(null);
-	const projectRef = useRef<HTMLDivElement>(null);
+  const cardRef = useRef<HTMLDivElement>(null);
+  const projectRef = useRef<HTMLDivElement>(null);
 
-	useLayoutEffect(() => {
-		let ctx = gsap.context(() => {
-			const tl = gsap.timeline();
+  useLayoutEffect(() => {
+    let ctx = gsap.context(() => {
+      const tl = gsap.timeline();
 
-			tl.fromTo(
-				projectRef.current,
-				{
-					xPercent: 100,
-				},
-				{
-					xPercent: 0,
+      tl.fromTo(
+        projectRef.current,
+        {
+          scale: 0,
+        },
+        {
+          scale: 1,
 
-					transformOrigin: "right top",
-					scrollTrigger: {
-						trigger: cardRef.current,
-						start: "top bottom",
-						end: "top top",
-						scrub: 1,
-					},
-				},
-			).fromTo(
-				projectRef.current,
-				{ xPercent: 0 },
-				{
-					xPercent: -100,
-					transformOrigin: "left top",
-					backgroundColor: "red",
-					scrollTrigger: {
-						trigger: cardRef.current,
-						start: "bottom bottom",
-						end: "bottom top",
-						scrub: 1,
-					},
-				},
-			);
-		});
+          transformOrigin: "right top",
+          scrollTrigger: {
+            trigger: cardRef.current,
+            start: "top bottom",
+            end: "top top",
+            scrub: 1,
+          },
+        },
+      ).fromTo(
+        projectRef.current,
+        { scale: 1 },
+        {
+          scale: 0,
+          transformOrigin: "left top",
+          backgroundColor: "red",
+          scrollTrigger: {
+            trigger: cardRef.current,
+            start: "bottom bottom",
+            end: "bottom top",
+            scrub: 1,
+          },
+        },
+      );
+    });
 
-		return () => ctx.revert();
-	}, []);
-	return (
-		<div
-			ref={cardRef}
-			className="card  flex flex-col gap-10 px-10  h-[100vh] w-full top-0 "
-		>
-			<div className="flex border-y-2 text-white items-center  py-5 border-white justify-between ">
-				<h1 className="text-3xl">{title}</h1>
-			</div>
-			<div
-				ref={projectRef}
-				className="h-[calc(100vh-100px)]  w-full bg-neutral-500 rounded-md"
-			>
-				<Image className="h-full w-full object-cover" fill alt="" src={img} />
-			</div>
-		</div>
-	);
+    return () => ctx.revert();
+  }, []);
+  return (
+    <div
+      ref={cardRef}
+      className="card  flex flex-col gap-10 px-10  h-[100vh] w-full top-0 "
+    >
+      <div className="flex border-y-2 text-white items-center  py-5 border-white justify-between ">
+        <h1 className="text-3xl">{title}</h1>
+      </div>
+      <div
+        ref={projectRef}
+        className="h-[calc(100vh-100px)]  w-full bg-neutral-500 rounded-md"
+      >
+        <Image className="h-full w-full object-cover" fill alt="" src={img} />
+      </div>
+    </div>
+  );
 };
