dfef0e196108bbc4a3f09bae36d453e463660595
HrishikShaji <hrishikminishaji@gmail.com>
2024-03-29 17:53:18 +0530

    scrollback bug fixed

--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useEffect, useLayoutEffect, useRef } from "react";
-import gsap from "gsap";
+import gsap from "gsap/all";
 import { Headings } from "./Headings";
 import { data } from "@/lib/data";
 import ScrollTrigger from "gsap/dist/ScrollTrigger";
@@ -9,98 +9,78 @@ export const Skills = () => {
 	const skillRefs = useRef<(HTMLDivElement | null)[]>([]);
 	const overlayRefs = useRef<(HTMLDivElement | null)[]>([]);
 	const containerRef = useRef<HTMLDivElement>(null);
+	const tl = useRef(null);
 
+	const directions = [
+		"right",
+		"right",
+		"right",
+		"right",
+		"left",
+		"left",
+		"left",
+		"down",
+		"down",
+		"right",
+		"right",
+		"right",
+		"left",
+		"left",
+		"left",
+		"down",
+	];
+	const indexes = [0, 1, 2, 3, 7, 6, 5, 4, 8, 9, 10, 11, 15, 14, 13, 12];
+
+	gsap.registerPlugin(ScrollTrigger);
 	useLayoutEffect(() => {
 		let ctx = gsap.context(() => {
-			const tl = gsap.timeline({
-				scrollTrigger: {
-					trigger: containerRef.current,
-					pin: containerRef.current,
-					start: "top top",
-					end: "+=3000",
-					scrub: true,
-				},
-			});
-
-			function disappear(i: number) {
-				gsap.to(overlayRefs.current[i], {
-					opacity: 0,
-				});
-			}
-
-			const directions = [
-				"right",
-				"right",
-				"right",
-				"right",
-				"left",
-				"left",
-				"left",
-				"down",
-				"down",
-				"right",
-				"right",
-				"right",
-				"left",
-				"left",
-				"left",
-				"down",
-			];
-			const indexes = [0, 1, 2, 3, 7, 6, 5, 4, 8, 9, 10, 11, 15, 14, 13, 12];
-
 			function getValue(index: number) {
 				if (directions[index] === "right") {
-					return { x: -100, y: 0 };
+					return { x: -110, y: 0 };
 				} else if (directions[index] === "left") {
-					return { x: 100, y: 0 };
+					return { x: 110, y: 0 };
 				} else {
-					return { x: 0, y: -100 };
+					return { x: 0, y: -110 };
 				}
 			}
 
-			indexes.forEach((index) => {
-				tl.fromTo(
-					overlayRefs.current[index],
-					{
-						xPercent: getValue(index).x,
-						yPercent: getValue(index).y,
-					},
-					{
-						xPercent: 0,
-						yPercent: 0,
-					},
-				);
-			});
+			const tl = gsap.timeline().fromTo(
+				overlayRefs.current,
+				{
+					xPercent: 100,
+					yPercent: 100,
+				},
+				{
+					xPercent: 0,
+					yPercent: 0,
+				},
+			);
 
-			skillRefs.current.forEach((skill) => {
-				gsap.fromTo(
-					skill,
-					{
-						autoAlpha: 0,
-						y: 50,
-					},
-					{
-						duration: 1,
-						autoAlpha: 1,
-						y: 0,
-						delay: 0.5,
-						scrollTrigger: {
-							trigger: skill,
-							toggleActions: "play none none none",
-							once: true,
-						},
-					},
-				);
+			ScrollTrigger.create({
+				animation: tl,
+				trigger: containerRef.current,
+				pin: containerRef.current,
+				start: "top top",
+				end: "+=3000",
+				scrub: true,
+				onLeave: function(self) {
+					let start = self.start;
+					self.scroll(self.start);
+					self.disable();
+					self.animation.progress(1);
+					ScrollTrigger.refresh();
+					window.scrollTo(0, start);
+				}
 			});
 		}, containerRef);
 
-		return () => ctx.kill();
+		return () => ctx.revert();
 	}, []);
 
 	return (
 		<div ref={containerRef} className="pb-40 flex flex-col gap-10">
 			<Headings text="SKILLS" />
-			<div className="px-10  w-full place-items-center h-full text-white grid grid-cols-4 ">
+			<div className="px-10 gap-0 w-full place-items-center h-full text-white grid grid-cols-4 ">
 				{data.skills.data.map((item, i) => (
 					<div
 						key={i}
@@ -109,9 +89,9 @@ export const Skills = () => {
 					>
 						<div
 							ref={(el) => (overlayRefs.current[i] = el)}
-							className="h-full w-full absolute left-0 top-0 bg-blue-500 mix-blend-difference"
+							className="h-full w-full absolute left-0 top-0 bg-red-500 "
 						></div>
-						<h1>{item.name}</h1>
+						<h1 className="absolute z-10 text-neutral-900">{item.name}</h1>
 					</div>
 				))}
 			</div>
--- app/page.tsx
+++ app/page.tsx
@@ -8,23 +8,19 @@ import { Hero } from "./components/Hero";
 import { Intro } from "./components/Intro";
 import { Projects } from "./components/Projects";
 import { Skills } from "./components/Skills";
-import { ScrollTrigger } from "gsap/all";
 import gsap from "gsap";
 import { Timeline } from "./components/Timeline";
+import ScrollTrigger from "gsap/dist/ScrollTrigger";
 
 export default function Home() {
-  useEffect(() => {
-    gsap.registerPlugin(ScrollTrigger);
-  }, []);
-
   return (
     <main className="    bg-neutral-900">
       <Hero />
+      <Skills />
       <Intro />
       <Features />
       <About />
       <Timeline />
-      <Skills />
       <Projects />
       <Contact />
     </main>
