07683d11aaea794c9d9c27197a80674bb979dcdd
HrishikShaji <hrishikminishaji@gmail.com>
2024-03-30 11:03:25 +0530

    modified testimonial animation

--- app/components/Intro.tsx
+++ app/components/Intro.tsx
@@ -6,49 +6,48 @@ import gsap from "gsap";
 import ScrollTrigger from "gsap/dist/ScrollTrigger";
 
 export const Intro = () => {
-  const textRefs = useRef<(HTMLSpanElement | null)[]>([]);
-  const containerRef = useRef<HTMLDivElement>(null);
-  useLayoutEffect(() => {
-    let ctx = gsap.context(() => {
-      gsap.set(textRefs.current, {
-        xPercent: (i) => (i + 1) * 1000,
-      });
-      gsap.to(textRefs.current, {
-        xPercent: 0,
-        stagger: {
-          from: "start",
-          each: 0.05,
-        },
-        scrollTrigger: {
-          trigger: textRefs.current,
-          start: "top center",
-          once: true,
-          markers: true,
-        },
-      });
-    }, containerRef);
+	const textRefs = useRef<(HTMLSpanElement | null)[]>([]);
+	const containerRef = useRef<HTMLDivElement>(null);
+	useLayoutEffect(() => {
+		let ctx = gsap.context(() => {
+			gsap.set(textRefs.current, {
+				xPercent: (i) => (i + 1) * 1000,
+			});
+			gsap.to(textRefs.current, {
+				xPercent: 0,
+				stagger: {
+					from: "start",
+					each: 0.05,
+				},
+				scrollTrigger: {
+					trigger: textRefs.current,
+					start: "top center",
+					once: true,
+				},
+			});
+		}, containerRef);
 
-    return () => ctx.kill();
-  }, []);
-  return (
-    <div
-      ref={containerRef}
-      id={data.intro.id}
-      className="container   text-white w-full relative bg-neutral-900  flex flex-col overflow-hidden"
-    >
-      <h1 className="text-9xl font-audiowide mix-blend-difference text-red-500">
-        I CREATE{" "}
-        {["C", "O", "O", "L"].map((item, i) => (
-          <span
-            key={i}
-            className="text-white inline-block"
-            ref={(el) => (textRefs.current[i] = el)}
-          >
-            {item}
-          </span>
-        ))}
-        WEBPAGES
-      </h1>
-    </div>
-  );
+		return () => ctx.kill();
+	}, []);
+	return (
+		<div
+			ref={containerRef}
+			id={data.intro.id}
+			className="container   text-white w-full relative bg-neutral-900  flex flex-col overflow-hidden"
+		>
+			<h1 className="text-9xl font-audiowide mix-blend-difference text-red-500">
+				I CREATE{" "}
+				{["C", "O", "O", "L"].map((item, i) => (
+					<span
+						key={i}
+						className="text-white inline-block"
+						ref={(el) => (textRefs.current[i] = el)}
+					>
+						{item}
+					</span>
+				))}
+				WEBPAGES
+			</h1>
+		</div>
+	);
 };
--- app/components/Testimonials.tsx
+++ app/components/Testimonials.tsx
@@ -4,57 +4,66 @@ import gsap from "gsap/all";
 import { Headings } from "./Headings";
 import { data } from "@/lib/data";
 import ScrollTrigger from "gsap/dist/ScrollTrigger";
+import Image from "next/image";
 
 export const Testimonials = () => {
-  const testimonialRefs = useRef<(HTMLDivElement | null)[]>([]);
-  const overlayRefs = useRef<(HTMLDivElement | null)[]>([]);
-  const containerRef = useRef<HTMLDivElement>(null);
-  const tl = useRef(null);
+	const testimonialRefs = useRef<(HTMLDivElement | null)[]>([]);
+	const overlayRefs = useRef<(HTMLDivElement | null)[]>([]);
+	const containerRef = useRef<HTMLDivElement>(null);
+	const tl = useRef(null);
 
-  useLayoutEffect(() => {
-    let ctx = gsap.context(() => {
-      if (!containerRef.current) return;
-      const width = window.innerWidth - 40;
+	useLayoutEffect(() => {
+		let ctx = gsap.context(() => {
+			if (!containerRef.current) return;
+			const width = window.innerWidth - 40;
 
-      const gap = width / 6;
-      console.log(width);
-      const tl = gsap.timeline();
+			const gap = width / 6;
+			const tl = gsap.timeline();
 
-      testimonialRefs.current.forEach((el, i) => {
-        tl.fromTo(el, { xPercent: (i + 1) * 300 }, { xPercent: i * 30 });
-      });
+			testimonialRefs.current.forEach((el, i) => {
+				tl.fromTo(el, { xPercent: (i + 1) * 300 }, { xPercent: 0 });
+			});
 
-      ScrollTrigger.create({
-        animation: tl,
-        trigger: containerRef.current,
-        pin: containerRef.current,
-        start: "top top",
-        end: "+=3000",
-        scrub: true,
-      });
-    }, containerRef);
+			ScrollTrigger.create({
+				animation: tl,
+				trigger: containerRef.current,
+				pin: containerRef.current,
+				start: "top top",
+				end: "+=3000",
+				scrub: true,
+			});
+		}, containerRef);
 
-    return () => ctx.revert();
-  }, []);
+		return () => ctx.revert();
+	}, []);
 
-  return (
-    <div ref={containerRef} className="h-screen  w-full flex flex-col gap-10">
-      <Headings text="TESTIMONIALS" />
-      <div className="p-10 overflow-hidden gap-0 relative w-full   h-full text-white  ">
-        {data.testimonials.map((item, i) => (
-          <div
-            key={i}
-            ref={(el) => (testimonialRefs.current[i] = el)}
-            className={`w-[500px] absolute h-[300px] bg-red-500 py-10 text-2xl font-semibold`}
-          >
-            <div
-              ref={(el) => (overlayRefs.current[i] = el)}
-              className="h-full w-full absolute left-0 top-0 bg-red-500 "
-            ></div>
-            <h1 className="absolute z-10 text-neutral-900">{item.name}</h1>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+	return (
+		<div ref={containerRef} className="h-screen  w-full flex flex-col gap-10">
+			<Headings text="TESTIMONIALS" />
+			<div className="p-10 overflow-hidden gap-0 relative w-full   h-full text-white  ">
+				{data.testimonials.map((item, i) => (
+					<div
+						key={i}
+						ref={(el) => (testimonialRefs.current[i] = el)}
+						style={{ left: `${(i + 1) * 100}px` }}
+						className={`w-[700px] absolute flex h-[300px]   text-2xl font-semibold`}
+					>
+						<div className="h-full p-5 w-[30%]  bg-red-500">
+							<Image
+								className="h-full flex-shrink-0 w-full object-cover"
+								height={1000}
+								width={1000}
+								alt=""
+								src={item.img}
+							/>
+						</div>
+						<div className="flex flex-col gap-2 w-[70%] bg-neutral-800 p-10">
+							<h1 className="">{item.name}</h1>
+							<p className="text-sm">{item.desc}</p>
+						</div>
+					</div>
+				))}
+			</div>
+		</div>
+	);
 };
